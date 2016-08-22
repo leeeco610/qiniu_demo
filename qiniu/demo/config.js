236830a3dac2e7435d02a1becc9e7939c584a51e
leeeco610 <759539395@qq.com>
2016-08-22 17:08:57 +0800

    七牛上传demo

---
+++ qiniu/demo/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'ACCESS_KEY': '',
+    'SECRET_KEY': '',
+    'Bucket_Name': '',
+    'Port': 8080,
+    'Uptoken_Url': 'uptoken',
+    'Domain': 'http://qiniu-plupload.qiniudn.com/'
+};
